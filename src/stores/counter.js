0714227dbba4ba1fe66b0a02a0ec37af200952a5
Salman-sajib <sfsajib@gmail.com>
2024-07-26 14:46:35 +0600

    added actions and getters to the store

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -2,9 +2,28 @@ import { ref, computed } from "vue";
 import { defineStore } from "pinia";
 
 export const useCounterStore = defineStore("counter", () => {
+  // states
   const count = ref(0);
+
+  // actions
+  function increaseCount() {
+    count.value++;
+  }
+
+  function decreaseCount() {
+    count.value--;
+  }
+
+  // getters
+  const oddOrEven = computed(() => {
+    if (count.value % 2 === 0) return "even";
+    return "odd";
+  });
 
   return {
     count,
+    increaseCount,
+    decreaseCount,
+    oddOrEven,
   };
 });
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,34 +2,18 @@
   <div class="home">
     <div class="count">{{ counterStore.count }}</div>
     <div class="buttons">
-      <button @click="decreaseCount">-</button>
-      <button @click="increaseCount">+</button>
+      <button @click="counterStore.decreaseCount">-</button>
+      <button @click="counterStore.increaseCount">+</button>
     </div>
     <hr />
-    <div>The counter is: {{ oddOrEven }}</div>
+    <div>The counter is: {{ counterStore.oddOrEven }}</div>
   </div>
 </template>
 
 <script setup>
-import { computed, ref } from "vue";
 import { useCounterStore } from "@/stores/counter";
 
 const counterStore = useCounterStore();
-
-const count = ref(0);
-
-function increaseCount() {
-  count.value++;
-}
-
-function decreaseCount() {
-  count.value--;
-}
-
-const oddOrEven = computed(() => {
-  if (count.value % 2 === 0) return "even";
-  return "odd";
-});
 </script>
 
 <style scoped>
