68ef5622dae5d5e9e9f19f666a988998e0fa5d6d
Salman-sajib <sfsajib@gmail.com>
2024-07-26 15:00:32 +0600

    shared the store data to about component

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="about">
-    <h1>About</h1>
+    <h2>This is the about page</h2>
+    <h1>{{ counterStore.count }}</h1>
+    <button @click="counterStore.increaseCount">Increase Count</button>
   </div>
 </template>
+
+<script setup>
+import { useCounterStore } from "@/stores/counter";
+
+const counterStore = useCounterStore();
+</script>
+
+<style scoped>
+button {
+  padding-inline: 1em;
+  padding-block: 0.7em;
+  border: none;
+  background-color: rgb(45, 90, 252);
+  color: white;
+  font-size: 1rem;
+  border-radius: 0.3em;
+  cursor: pointer;
+}
+</style>
