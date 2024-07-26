ec21cea26479a3f8c215ddbb7fcee88c9eedc8ec
Salman-sajib <sfsajib@gmail.com>
2024-07-26 12:33:21 +0600

    added the counter and a check for even or odd value

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,41 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <div class="count">{{ count }}</div>
+    <div class="buttons">
+      <button @click="decreaseCount">-</button>
+      <button @click="increaseCount">+</button>
+    </div>
+    <hr />
+    <div>The counter is: {{ oddOrEven }}</div>
   </div>
 </template>
+
+<script setup>
+import { computed, ref } from "vue";
+
+const count = ref(0);
+
+function increaseCount() {
+  count.value++;
+}
+
+function decreaseCount() {
+  count.value--;
+}
+
+const oddOrEven = computed(() => {
+  if (count.value % 2 === 0) return "even";
+  return "odd";
+});
+</script>
+
+<style scoped>
+.count {
+  font-size: 4.5rem;
+}
+
+.buttons button {
+  font-size: 2.5rem;
+  margin: 10px;
+}
+</style>
